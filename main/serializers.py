bebe5d63da7c227b2a00d70d1cf7d770b9ea91d1
Nand Kishore <knand4930@gmail.com>
2023-04-15 10:11:48 +0530

    login and register with otp

--- main/serializers.py
+++ main/serializers.py
@@ -1,8 +1,30 @@
+import imp
 from rest_framework import serializers
-from .models import Profile, User, PlanUpdate
-
+from .models import Profile, User, PlanUpdate, Role
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 # class UserUpdateSerializer()
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "fname", "lname", "phone" ,"email","address","city","state","country","image","image"]
+        extra_kwargs = {
+            "id": {"read_only": True},
+            "fname": {"required": True},
+            "lname": {"required": True},
+            "phone": {"required": True},
+            "email": {"required": True},
+        }
+
+
+    def create(self, validated_data):
+        user = User.objects.create(**validated_data)
+        return user
+
 
 class UserUpdateSerializer(serializers.ModelSerializer):
     class Meta:
--- main/urls.py
+++ main/urls.py
@@ -1,14 +1,23 @@
-from django.urls import path, re_path
-from .views import *
+from django.urls import path, re_path, include
+from main.views import *
+from rest_framework import routers
+from knox.views import LogoutView
+
+
+router = routers.DefaultRouter()
+router.register(r'user-register', RegisterView, basename='task')
 
 urlpatterns = [
-    path('api/register/', RegisterView.as_view(), name='register_view'),
-    path('api/register/otp/', otp, name="otp_register"),
-    path('api/login/attempt/', LoginAttemptView.as_view(), name='login_attempt'),
-    path('api/login/otp/', LoginOtpView.as_view(), name='login_otp'),
+    path('', include(router.urls)),
+    path('get-login-otp-mobile/', ValidatePhoneSendOTP.as_view(), name='get-login-otp-mobile'),
+    path('verify-login-otp-mobile/', VerifyPhoneOTPView.as_view(), name='login-otp-verify'),
+    path('logout/', LogoutView.as_view(), name='knox_logout'),
+    # path('api/register/', RegisterView.as_view(), name='register_view'),
+    # path('api/register/otp/', otp, name="otp_register"),
+    # path('api/login/attempt/', LoginAttemptView.as_view(), name='login_attempt'),
+    # path('api/login/otp/', LoginOtpView.as_view(), name='login_otp'),
     path('api/user/update/<int:user_id>', UserRetrieveUpdateAPIView.as_view(), name='user_update'),
     path('api/user/profile/<int:pk>', UserProfile.as_view(), name="user_profile"),
     path('api/user/plan/update/', PlanUpdateAPIView.as_view(), name='PlanUpdateAPIView'),
 
 ]
-
