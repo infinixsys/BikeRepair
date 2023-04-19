4507a2ed87b20f338115f67062587742f3033556
Ubuntu <ubuntu@ip-172-31-32-185.ap-northeast-1.compute.internal>
2023-04-19 05:19:42 +0000

    udpated

---
+++ static/assets/js/script.js
@@ -0,0 +1,19 @@
+// let active = document.querySelectorAll('.nav-link');
+// const activepage = window.location.pathname;
+// for (key of active) {
+//    if (key.href.includes(`${activepage}`)) {
+//       key.classList.add('bg-dark');
+//       key.classList.replace('link-dark', 'link-light');
+//    }
+//    // its is for option and admin // if all file in one main directory
+//    if (window.location.pathname === "/stpaul/") {
+//      console.log(`${activepage}`);
+//       key.classList.remove('bg-dark');
+//       key.classList.replace('link-light', 'link-dark');
+//       active[0].classList.add("bg-dark");
+//       active[0].classList.replace('link-dark', 'link-light');
+//    }
+// };
+
+
+
