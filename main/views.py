c59becbb20ecebaaa901d146ee9f3da46253f8a3
Nand Kishore <knand4930@gmail.com>
2023-05-04 14:51:32 +0530

    update the otp verifications

--- main/views.py
+++ main/views.py
@@ -124,8 +124,8 @@ def send_otp(phone):
         key = otp_generator()
         phone = str(phone)
         otp_key = str(key)
-
-        link = f'https://2factor.in/API/R1/?module=TRANS_SMS&apikey=7c59cf94-d129-11ec-9c12-0200cd936042&to={phone}&from=MMBook&templatename=mymedbook&var1={otp_key}&var2={otp_key}'
+        API_Key = "XVqxz6mGg8Z24uiDenBAYPIb0KpMJjQHNlWE1f3vSF9Os7UTkyKkgiT2owNJFLtS4XjZGbuIyxr9Pzf5"
+        link = f'https://www.fast2sms.com/dev/bulkV2?authorization={API_Key}&variables_values={otp_key}&route=otp&numbers={phone}'
 
         result = requests.get(link, verify=False)
 
