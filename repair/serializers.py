4fc22fccbccd9d061fabb0850d29c28447bce9c5
Nand Kishore <56184819+knand4930@users.noreply.github.com>
2023-05-29 12:25:21 +0530

    Update serializers.py
    update plan serializers

--- repair/serializers.py
+++ repair/serializers.py
@@ -17,7 +17,7 @@ class AboutUsSerializer(serializers.ModelSerializer):
 class PlanNameSerializer(serializers.ModelSerializer):
     class Meta:
         model = PlanName
-        fields = ('id', 'title', 'pricing', 'types', 'details', 'card_details', 'img', 'status', 'line_price')
+        fields = "__all__"
 
 
 class NotificationSerializer(serializers.ModelSerializer):
